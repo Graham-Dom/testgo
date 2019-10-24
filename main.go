9bdcf1d93297bff81fade3c5d765ebc09cf87c7f
Graham Dominick <graham.dom17@gmail.com>
2019-10-24 09:47:34 -0600

    First

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"log"
+	"module"
+)
+
+func main() {
+	log.Println("Hello World")
+	module.print_hello()
+}
---
+++ module.go
@@ -0,0 +1,9 @@
+package module
+
+import (
+	"log"
+)
+
+func print_hello(){
+	log.Println("Hello")
+}
